2e9b1c8b8970542b4685a5251439a6627eea36ec
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-07-08 00:08:40 +0600

    Add Fetch from API component and use different states to display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import SomeFunction from './components/GetApi';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <SomeFunction />
     </div>
   );
 }
---
+++ src/components/GetApi.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+
+const SomeFunction = () => {
+  const url = 'https://api.api-ninjas.com/v1/quotes?category=business';
+  const key = 'bvuPSGiToRh49Pmg7okBJQ==vaRCLzbhx7mll6yh';
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [quote, setQuote] = useState('');
+
+  const getApi = async () => {
+    try {
+      setLoading(true);
+
+      const response = await fetch(url, {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Api-Key': key,
+        },
+      });
+
+      const getQuote = await response.json();
+      console.log(getQuote);
+      setQuote(getQuote[0]);
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    getApi();
+  }, []);
+
+  if (error) return <div>Error fetching Data</div>;
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <p>{quote.quote}</p>
+      <p>{`- ${quote.author}`}</p>
+    </div>
+  );
+};
+
+export default SomeFunction;
